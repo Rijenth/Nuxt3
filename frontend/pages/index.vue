7ec8b4458c82f06ea739e7b13f7539e14c3a3eff
rijenth <rijenth93100@laposte.net>
2023-05-01 21:49:30 +0200

    fix hmr hotreloading

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <h1 class="font-bold">If this is bold, Tailwind is working!</h1>
-        <p>Count bonjour : {{ count }}
+        <p>Count : {{ count }}
             <button @click="exampleStore.increment" type="submit" class="bg-black text-white font-bold px-2 mx-2 rounded">+</button>
             if the count is incrementing, pinia is working!
         </p>
