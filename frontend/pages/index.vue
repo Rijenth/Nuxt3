1a473d1c04608e52238ea1c7d8d113b043bf3ea0
rijenth <rijenth93100@laposte.net>
2023-05-01 21:17:57 +0200

    init Nuxt3 - Tailwind - Pinia

---
+++ frontend/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <h1 class="font-bold">If this is bold, Tailwind is working!</h1>
+        <p>Count bonjour : {{ count }}
+            <button @click="exampleStore.increment" type="submit" class="bg-black text-white font-bold px-2 mx-2 rounded">+</button>
+            if the count is incrementing, pinia is working!
+        </p>
+      <NuxtWelcome />
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import { useIncrementStore } from '~/store/increment';
+    import { storeToRefs } from 'pinia';
+
+    const exampleStore = useIncrementStore();
+
+    const { count } = storeToRefs(exampleStore);    
+</script>
+  
